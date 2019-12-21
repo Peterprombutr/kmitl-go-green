387587fbc83385ae80b7155e83ca28ca8eb77056
PPsyrius <PPsyrius@users.noreply.github.com>
2019-12-22 03:28:05 +0700

    Submission ver.

--- ass/KGG_RPi_camera_snapshot.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-
-camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/Desktop/GoGreen/last_snapshot.jpg')
-camera.stop_preview()
